6d949e9c16fe9d4e6da1f560c8b15e562b86c8ec
Fábio Mendes <fabiobends@gmail.com>
2023-01-06 20:51:33 -0300

    feat(brackets-matcher): add solution
    Coderbyte challenge, it is not the best solution but it worked pretty
    well! This time trying Dart language!

---
+++ day-006/brackets-matcher.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+String BracketMatcher(String str) {
+  // code goes here
+  RegExp exp = RegExp(r'[a-zA-Z]');
+  str = str.replaceAll(exp, '');
+  str = str.replaceAll(' ', '');
+  var match = true;
+  while(match){
+    var oldStr = str;
+    str = str.replaceAll('()', '');
+    if(oldStr == str){
+      match = false;
+    }
+  }
+  if (str.length > 0) return '0';
+  return '1';
+}
+   
+// keep this function call here 
+void main() {
+  print(BracketMatcher(stdin.readLineSync()));
+}
