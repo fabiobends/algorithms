12370f424b56049a6857265c38b9dff3700e26b4
Fábio Mendes <fabiobends@gmail.com>
2023-01-16 08:53:56 -0300

    feat(zigzag-conversion): add solution
    Leetcode challenge, this is the proper solution using Dart!

--- day-013/zigzag-conversion.dart
+++ day-013/zigzag-conversion.dart
@@ -1,30 +1,20 @@
 class Solution {
   String convert(String s, int numRows) {
-    Map<int, String?> rowString = Map();
-    String newString = "";
+    final rowStrings = List<String>.filled(numRows, "");
     for (int i = 0; i < s.length; i++){
       final row = getRowNumber(i, numRows);
-      if(rowString[row] == null){
-        rowString[row] = s[i];
-      } else {
-        rowString[row] = '${rowString[row]}${s[i]}';
-      }
+      rowStrings[row] += + s[i];
     }
-    for (int i = 0; i < numRows; i++){
-      newString += '${rowString[i]}';
-    }
-    return newString;
+    return rowStrings.join("");
   }
 
   int getRowNumber(int index, int numRows){
-    if (index < numRows){
-      return index;
+    if (numRows == 1) return 0;
+    final vShapeLength = numRows * 2 - 2;
+    final rowCandidate = index % vShapeLength;
+    if (rowCandidate >= numRows){
+      return vShapeLength - rowCandidate;
     }
-    final vShapeLength = (numRows * 2 - 1);
-    final mod = (index + 1) % vShapeLength;
-    if (mod > numRows){
-      return vShapeLength - mod;
-    }
-    return mod;
+    return rowCandidate;
   }
 }
