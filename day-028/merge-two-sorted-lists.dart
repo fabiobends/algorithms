8b8eab00f730953bffe61a793be5cff4a6ed9c66
Fábio Mendes <fabiobends@gmail.com>
2023-02-01 21:48:24 -0300

    feat(merge-two-sorted-lists): add solution
    Nice and efficient solution!
    
    ref: https://leetcode.com/problems/merge-two-sorted-lists

---
+++ day-028/merge-two-sorted-lists.dart
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+    ListNode head = ListNode();
+    ListNode? pointer = head;
+    while (list1 != null && list2 != null){
+      var firstValue = list1?.val ?? 0;
+      var secondValue = list2?.val ?? 0;
+      if (firstValue > secondValue){
+        pointer?.next = list2;
+        list2 = list2?.next;
+      } else {
+        pointer?.next = list1;
+        list1 = list1?.next;
+      }
+      pointer = pointer?.next;
+    }
+    if (list1 == null){
+      pointer?.next = list2;
+    } else {
+      pointer?.next = list1;
+    }
+    return head.next;
+  }
+}
