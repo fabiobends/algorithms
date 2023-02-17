5a9d67af9b894d5b6818144a16efb101c5f395cb
Fábio Mendes <fabiobends@gmail.com>
2023-02-17 10:47:32 -0300

    feat(mininum-distance-between-bst-nodes): add solution
    ref: https://leetcode.com/problems/minimum-distance-between-bst-nodes

---
+++ src/day-043/minimum-distance-between-bst-nodes.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  int minDistance = pow(10, 5).toInt();
+  // Initially, it will be null.
+  TreeNode? prevValue;
+
+  void inorderTraversal(TreeNode? root) {
+    if (root == null) {
+      return;
+    }
+    inorderTraversal(root.left);
+    // Find the difference with the previous value if it is there.
+    if (prevValue != null) {
+      minDistance = min(minDistance, (root?.val ?? 0) - (prevValue?.val ?? 0));
+    }
+    prevValue = root;
+    inorderTraversal(root.right);
+  }
+
+  int minDiffInBST(TreeNode? root) {
+    inorderTraversal(root);
+    return minDistance;
+  }
+}
