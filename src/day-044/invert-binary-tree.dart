436bb982a53771a5efddafddde72920fa0edeeef
Fábio Mendes <fabiobends@gmail.com>
2023-02-18 11:03:46 -0300

    feat(invert-binary-tree): add solution
    Solution using recursion, pretty easy.
    
    ref: https://leetcode.com/problems/invert-binary-tree

---
+++ src/day-044/invert-binary-tree.dart
@@ -0,0 +1,19 @@
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class Solution {
+  TreeNode? invertTree(TreeNode? root) {
+    if (root == null) {
+      return null;
+    }
+    TreeNode? leftNode = invertTree(root?.left);
+    TreeNode? rightNode = invertTree(root?.right);
+    root?.left = rightNode;
+    root?.right = leftNode;
+    return root;
+  }
+}
