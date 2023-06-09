252a020a78747ddc52f29b678b69e4aa438182ce
Fábio Mendes <fabiobends@gmail.com>
2023-06-09 11:18:38 -0300

    feat(find-smallest-letter-greater-than-target): add solution
    ref: https://leetcode.com/problems/find-smallest-letter-greater-than-target

---
+++ src/60-days-03/day-135/find-smallest-letter-greater-than-target.kt
@@ -0,0 +1,18 @@
+class Solution {
+  fun nextGreatestLetter(letters: CharArray, target: Char): Char {
+    var result = letters[0]
+    if (result > target || letters.last() <= target) return result
+    var left = 0
+    var right = letters.lastIndex
+    while (left <= right) {
+      val mid = (left + right) / 2
+      if (letters[mid] > target){
+        result = letters[mid]
+        right = mid - 1
+      } else {
+        left = mid + 1
+      }
+    }
+    return result
+  }
+}
